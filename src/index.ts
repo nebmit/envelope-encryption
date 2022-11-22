e663ecd2fdfbe4e4c71ce91d89bc55e7a3530a5e
mäh <76664673+nebmit@users.noreply.github.com>
2022-11-22 20:58:01 +0100

    initial

---
+++ src/index.ts
@@ -0,0 +1,152 @@
+import {
+  randomBytes,
+  createCipheriv,
+  createDecipheriv,
+  pbkdf2Sync,
+} from 'crypto';
+
+const iterations = 320000;
+const keyLength = 32;
+const saltLength = 16;
+const ivLength = 16;
+
+interface encryption {
+  wrappedDEK: string;
+  dekIv: string;
+  dekAuthTag: string;
+  KEK: string;
+  saltKEK: string;
+}
+
+interface encryptedData {
+  data: string;
+  iv: string;
+  authTag: string;
+}
+
+/**
+ * Generate all encryption requirements.
+ * @param keyphrase The keyphrase to use for this encryption
+ * @returns The encryption object
+ */
+function initialize(keyphrase: string): encryption {
+  var saltKEK = randomBytes(saltLength).toString('hex');
+
+  var KEK = pbkdf2Sync(keyphrase, saltKEK, iterations, keyLength, 'sha512');
+  var DEK = randomBytes(keyLength);
+  var dekIv = randomBytes(ivLength).toString('hex');
+
+  var encrypted = _encrypt(DEK, KEK, dekIv);
+
+  DEK.fill(0);
+
+  return {
+    wrappedDEK: encrypted.data,
+    dekIv: dekIv,
+    dekAuthTag: encrypted.authTag,
+    KEK: KEK.toString('hex'),
+    saltKEK: saltKEK,
+  };
+}
+
+/**
+ * Generate the Key Encryption Key (KEK) from the keyphrase.
+ * @param keyphrase The keyphrase used to initialize the encryption
+ * @param saltKEK The salt used to generate the key encryption key
+ * @returns The key encryption key
+ */
+function generateKey(keyphrase: string, saltKEK: string): string {
+  const KEK = pbkdf2Sync(keyphrase, saltKEK, iterations, keyLength, 'sha512');
+  return KEK.toString('hex');
+}
+
+/**
+ * Encrypt data for storage
+ * @param dataToEncrypt The data to encrypt
+ * @param wrappedDEK The wrapped data encryption key
+ * @param dekIv The initialization vector used to encrypt the data encryption key
+ * @param dekAuthTag The authentication tag used to encrypt the data encryption key
+ * @param KEK The key encryption key
+ * @returns Encrypted data
+ */
+function encryptData(
+  dataToEncrypt: string,
+  wrappedDEK: string,
+  dekIv: string,
+  dekAuthTag: string,
+  KEK: string
+): encryptedData {
+  var bufferKEK = Buffer.from(KEK, 'hex');
+  var DEK = _decrypt(wrappedDEK, bufferKEK, dekIv, dekAuthTag);
+  var dataIv = randomBytes(ivLength).toString('hex');
+  var encrypted = _encrypt(Buffer.from(dataToEncrypt), DEK, dataIv);
+  DEK.fill(0);
+  return encrypted;
+}
+
+/**
+ * Decrypt stored data
+ * @param dataToDecrypt The encrypted data
+ * @param decryptionAuthTag The authentication tag used to encrypt the data
+ * @param decryptionIv The initialization vector used to encrypt the data
+ * @param wrappedDEK The wrapped data encryption key
+ * @param dekIv The initialization vector used to encrypt the data encryption key
+ * @param dekAuthTag The authentication tag used to encrypt the data encryption key
+ * @param KEK The key encryption key
+ * @returns The decrypted data
+ */
+function decryptData(
+  dataToDecrypt: string,
+  decryptionAuthTag: string,
+  decryptionIv: string,
+  wrappedDEK: string,
+  dekIv: string,
+  dekAuthTag: string,
+  KEK: string
+): string {
+  var bufferKEK = Buffer.from(KEK, 'hex');
+  var DEK = _decrypt(wrappedDEK, bufferKEK, dekIv, dekAuthTag);
+  var decryptedData = _decrypt(
+    dataToDecrypt,
+    DEK,
+    decryptionIv,
+    decryptionAuthTag
+  );
+  DEK.fill(0);
+  return decryptedData.toString();
+}
+
+function _encrypt(
+  dataToEncrypt: Buffer,
+  key: Buffer,
+  iv: string
+): encryptedData {
+  const cipher = createCipheriv('aes-256-gcm', key, iv);
+  const encrypted = Buffer.concat([
+    cipher.update(dataToEncrypt),
+    cipher.final(),
+  ]);
+  const authTag = cipher.getAuthTag().toString('hex');
+  return {
+    data: encrypted.toString('hex'),
+    authTag: authTag,
+    iv: iv,
+  };
+}
+
+function _decrypt(
+  dataToDecrypt: string,
+  key: Buffer,
+  iv: string,
+  authTag: string
+): Buffer {
+  const decipher = createDecipheriv('aes-256-gcm', key, iv);
+  decipher.setAuthTag(Buffer.from(authTag, 'hex'));
+  const decrypted = Buffer.concat([
+    decipher.update(Buffer.from(dataToDecrypt, 'hex')),
+    decipher.final(),
+  ]);
+  return decrypted;
+}
+
+export { initialize, generateKey, encryptData, decryptData };
---
+++ test/index.test.ts
@@ -0,0 +1,44 @@
+import {
+  decryptData,
+  encryptData,
+  generateKey,
+  initialize,
+} from '../src/index';
+
+test('initialize', () => {
+  const encryption = initialize('test');
+  expect(encryption.wrappedDEK).toBeDefined();
+  expect(encryption.dekIv).toBeDefined();
+  expect(encryption.dekAuthTag).toBeDefined();
+  expect(encryption.KEK).toBeDefined();
+  expect(encryption.saltKEK).toBeDefined();
+});
+
+test('get kek', () => {
+  const encryption = initialize('test');
+  const KEK = generateKey('test', encryption.saltKEK);
+  expect(KEK).toBe(encryption.KEK);
+});
+
+test('encrypt and decrypt data', () => {
+  const encryption = initialize('test');
+  const encrypted = encryptData(
+    'test',
+    encryption.wrappedDEK,
+    encryption.dekIv,
+    encryption.dekAuthTag,
+    encryption.KEK
+  );
+  expect(encrypted.data).toBeDefined();
+  expect(encrypted.authTag).toBeDefined();
+  const decrypted = decryptData(
+    encrypted.data,
+    encrypted.authTag,
+    encrypted.iv,
+    encryption.wrappedDEK,
+    encryption.dekIv,
+    encryption.dekAuthTag,
+    encryption.KEK
+  );
+  expect(decrypted).toBe('test');
+});
